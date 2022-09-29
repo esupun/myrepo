4f4831da7017568f5142f2235d2e231067c4aa3f
esupun <96661355+esupun@users.noreply.github.com>
2022-09-29 23:33:43 +0530

    Update code.py

--- STO/code.py
+++ STO/code.py
@@ -13,23 +13,23 @@ platform = 'juniper_junos'
 port = '22'
 
 with open(hostfile) as f:
-    for line in f:
-        host = line.strip()
-        try:
-            print ("\nConnecting ")
-	    net_connect = ConnectHandler(device_type=platform, ip=host, username=username, password=password, port = port) 
-	    print ('\nConnected to {}, gathering info...\n'.format(host))
+	for line in f:
+		host = line.strip()
+		try:
+			print ("\nConnecting ")
+			net_connect = ConnectHandler(device_type=platform, ip=host, username=username, password=password, port = port) 
+			print ('\nConnected to {}, gathering info...\n'.format(host))
 
 with open(conf) as f:
-   for line in f:
-      command=line.strip()
-      try:
-	  print(command)
-	  output = net_connect.send_command(command)
-	  print(output)
+	for line in f:
+		command=line.strip()
+		try:
+			print(command)
+			output = net_connect.send_command(command)
+			print(output)
 
-      except:
-          print("Error running {} command".format(command))
+		except:
+			print("Error running {} command".format(command))
 
-      except:
-          print ('Error Connecting {}'.format(host)) 
+		except:
+			print ('Error Connecting {}'.format(host)) 
