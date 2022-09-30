23fbad46ea8b33b09387147b5104a3d22d7d5fd7
esupun <96661355+esupun@users.noreply.github.com>
2022-09-30 07:28:38 +0530

    Update code.py

--- STO/code.py
+++ STO/code.py
@@ -28,6 +28,7 @@ with open(hostfile) as f:
 						output = net_connect.send_command(command)
 						print(output)
 						log_init = int(output.split()[4].split('%')[0])
+						print(log_init)
 						if log_init < 80:
 							com_out = net_connect.send_command_timing(command_string = 'request system storage cleanup')
 							if "Delete these files" in com_out:
